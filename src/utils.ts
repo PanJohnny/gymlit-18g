1ee95e5d5b521bbb12efb17002ad010e619e799e
PanJohnny <devstefancz@gmail.com>
2022-11-22 19:40:39 +0100

    Zpraveny cookies!

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,3 @@
-export function createExpires(days:number) {
-    var date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    return date;
-}
-
 export function validYear(c:string) {
     return c && /[0-8]/g.test(c) && c.length == 1
 }
@@ -11,3 +5,5 @@ export function validYear(c:string) {
 export function className(c:string) {
     return c != "0" ? "Třída: " + c + ". G" : "Třída: 1.G - 8.G"
 }
+
+export const cookieOptions = {maxAge: 24*60*60, sameSite: true, path: "/"}
