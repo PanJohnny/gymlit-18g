20a10aaf0c1a30f3dac3c59a7bea48f070d9ca4a
PanJohnny <devstefancz@gmail.com>
2022-11-22 19:48:23 +0100

    možný fix

--- src/utils.ts
+++ src/utils.ts
@@ -6,4 +6,4 @@ export function className(c:string) {
     return c != "0" ? "Třída: " + c + ". G" : "Třída: 1.G - 8.G"
 }
 
-export const cookieOptions = {maxAge: 24*60*60, sameSite: true, path: "/"}
+export const cookieOptions = {maxAge: 24*60*60, path: "/"}
