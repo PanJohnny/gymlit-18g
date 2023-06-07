5253ff14c45f8658d5130ce8f050d6288f5d12b4
Mona Lisa <devstefancz@gmail.com>
2023-06-07 09:22:35 +0200

    Stránky viditelné pro roboty

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-    output: "server",
-    adapter: vercel()
+  output: "server",
+  adapter: vercel(),
+  site: 'https://panjohnny.vercel.app',
+  integrations: [sitemap({
+    filter: (page => page.includes("kronika") || page.includes("events"))
+  })]
 });
